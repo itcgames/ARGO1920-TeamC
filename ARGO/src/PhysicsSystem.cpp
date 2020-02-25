8a8aae7fbb948be441f26299ffc21ae4bdc89585
unknown <C00220950@itcarlow.ie>
2020-02-25 19:00:14 +0000

    changed friction

--- ARGO/src/PhysicsSystem.cpp
+++ ARGO/src/PhysicsSystem.cpp
@@ -26,7 +26,7 @@ void PhysicsSystem::update(Entity& t_entity, float t_dt)
 				glm::vec2 friction = forceComp->getForce() * FRICTION_SCALAR;
 				glm::vec2 dif = forceComp->getForce() - friction;
 				//this might have to be multiplied by t_dt
-				forceComp->setForce(forceComp->getForce() * FRICTION_SCALAR);
+				forceComp->setForce(forceComp->getForce() - dif);
 			}
 		}
 		handleRotation(transformComp);
